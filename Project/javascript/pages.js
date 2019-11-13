877b70a8696d88b0b0facaab6f133ba5877e8513
hemanthtallapaneni <tallapaneni.hemanth@gmail.com>
2019-11-14 00:05:47 +0530

    change

---
+++ Project/javascript/pages.js
@@ -0,0 +1,13 @@
+<script>
+$("#demo").datetimepicker({
+});
+    
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
